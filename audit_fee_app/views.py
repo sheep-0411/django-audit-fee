3ee227dacb90f4a7a5afeb6fdb0f1182c7a61012
sheep <yuta.k10ic@gmail.com>
2022-01-08 17:10:52 +0900

    make it better

--- audit_fee_app/views.py
+++ audit_fee_app/views.py
@@ -8,6 +8,45 @@ from django.db.models import Q
 from django.db.models import Count
 from django.db.models import Max
 
+
+# Googleスプレッドシートとの連携に必要なライブラリ
+import gspread
+from dotenv import find_dotenv, load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+
+# 設定
+file_name = 'audit-fee-bot'
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+# .envファイルを探して読み込む
+load_dotenv(find_dotenv())
+
+# 辞書オブジェクト。認証に必要な情報をHerokuの環境変数から呼び出している
+credential = {
+"type": "service_account",
+"project_id": os.environ['SHEET_PROJECT_ID'],
+"private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+"private_key": os.environ['SHEET_PRIVATE_KEY'],
+"client_email": os.environ['SHEET_CLIENT_EMAIL'],
+"client_id": os.environ['SHEET_CLIENT_ID'],
+"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+"token_uri": "https://oauth2.googleapis.com/token",
+"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+"client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+}
+# スプレッドシートにアクセス
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
+gc = gspread.authorize(credentials)
+sh = gc.open(file_name)
+
+# シートから全部から読み込み
+def get_records(wks):
+    record = pd.DataFrame(wks.get_all_records())
+    return record
+
 def top(request):
     audit_fee = Audit_fee.objects.all()
     audit_firm = Audit_fee.objects.values('auditor').annotate(dcount=Count('auditor'))
@@ -22,7 +61,11 @@ def top(request):
         )
     
     df = pd.DataFrame(list(audit_fee.values()))
-    print(df)
+    print(type(df))
+
+    wks5 = sh.worksheet('save_data')
+    df5 = get_records(wks5).head(10)
+    print(df5)
 
     context = {
         'audit_fee':audit_fee,
